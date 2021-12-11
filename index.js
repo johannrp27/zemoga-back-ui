5fc2b3496c90d1d133154ed9543579c5fcb8308b
Johann Rojas <jrojas@modyo.com>
2021-12-10 23:34:10 -0500

    chore: add comment to start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 const app = express()
-const PORT = 8083
+const PORT = 8089
 
 app.use(express.json())
 app.use(cors());
@@ -15,7 +15,15 @@ app.get('/', (req, res) => {
 })
 
 routerApi(app)
-
+/*
+  Use this if you want to serve this over network:
+    app.listen(PORT, '192.168.1.12', () => {
+    ...
+*/
 app.listen(PORT, () => {
-  console.log(`Running on http://localhost:${PORT}`)
+  console.log(`
+  App running at
+    - Local: http://localhost:${PORT}
+    - Network: http://192.168.1.12:${PORT}
+  `)
 })
